2878fd4ce75fe428f0c168a8787d510d35c0163f
Mark Richardson <markeban@yahoo.com>
2016-04-18 15:50:48 -0700

    changed instance variable names @taco to @product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,36 +1,36 @@
 class ProductsController < ApplicationController
 
 def index
-  @tacos = Product.all
+  @products = Product.all
   
 end
 
 def show
-  @taco = Product.find_by(id: params[:id])
+  @product = Product.find_by(id: params[:id])
 end
 
 def new
 end
 def create
-  @taco = Product.create(id: params[:id], name: params[:name], price: params[:price], image: params[:image], description: params[:description], rating: params[:rating])
+  @product = Product.create(id: params[:id], name: params[:name], price: params[:price], image: params[:image], description: params[:description], rating: params[:rating])
   flash[:success] = "Taco made!"
-  redirect_to "/products/#{@taco.id}"
+  redirect_to "/products/#{@product.id}"
 end
 
 def edit
-  @taco = Product.find_by(id: params[:id])
+  @product = Product.find_by(id: params[:id])
 end
 
 def update
-  @taco = Product.find_by(id: params[:id])
-  @taco.update(id: params[:id], name: params[:name], price: params[:price], image: params[:image], description: params[:description], rating: params[:rating])
+  @product = Product.find_by(id: params[:id])
+  @product.update(id: params[:id], name: params[:name], price: params[:price], image: params[:image], description: params[:description], rating: params[:rating])
   flash[:success] = "This taco has been updated!"
-  redirect_to "/products/#{@taco.id}"
+  redirect_to "/products/#{@product.id}"
 end
 
 def destroy
-  @taco = Product.find_by(id: params[:id])
-  @taco.destroy
+  @product = Product.find_by(id: params[:id])
+  @product.destroy
   flash[:warning] = "Taco destroyed!"
   redirect_to "/"
 end
